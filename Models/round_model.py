c8d41a0ab45386960ee1fb4a6035757494ae8824
Carole Roch <1rochcarole@gmail.com>
2025-02-11 13:36:44 +0100

    add .gitignore

--- Models/round_model.py
+++ Models/round_model.py
@@ -2,9 +2,6 @@ class Round:
     def __init__(self, name_round, start_time, end_time, matches):
         '''initalizing a round'''
         self.name_round = name_round
-        self.start_time = start_time
-        self.end_time = end_time
-        self.matchs = matches
 
     def add_match(self):
         '''add a match to the round'''
